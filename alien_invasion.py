71478fe389893793092f1a3a17158b4eca291f1b
祝星 <142075210+index-null@users.noreply.github.com>
2023-12-07 17:49:43 +0800

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,14 +3,15 @@ import sys
 
 
 def run_game():
-    screen = pygame.display.set_mode((800, 1600))
+    screen = pygame.display.set_mode((800, 1200))
     pygame.display.set_caption('Alien Invasion')
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
-    pass
+    run_game()
